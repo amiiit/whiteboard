28de6cee862e97d4af5724b40302c1f489aad4eb
Amit <jakubowicz.amit@gmail.com>
2014-06-24 13:49:24 +0200

    start with serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -427,11 +427,17 @@ module.exports = function (grunt) {
         });
     });
 
-    grunt.registerTask('server', [
+
+    grunt.registerTask('serve', [
         'build',
         'connect:server',
         'watch'
     ]);
+
+    grunt.registerTask('server', function() {
+           grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+           grunt.task.run(['serve']);
+       });
 
     grunt.registerTask('unit-test', [
         'clean:test',
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 whiteboard
 ==========
+
+
+## Quick Start
+
+Install Node.js and then:
+
+```sh
+$ git clone git@github.com:nubo/whiteboard.git
+$ cd whiteboard
+$ sudo npm -g install grunt-cli karma bower
+$ npm install
+$ bower install
+$ grunt serve
+```
