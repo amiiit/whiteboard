fe6dde1339eaf3ef66626e89cc1fea1278ea599a
Amit <jakubowicz.amit@gmail.com>
2014-07-07 08:44:31 +0200

    gross changes reach router subscription

--- src/app/router/router-subscribtion.js
+++ src/app/router/router-subscribtion.js
@@ -1,10 +1,20 @@
 angular.module('nuBoard')
   .directive('nuRouterSubscription', function () {
     return {
-      controller: 'RouterCtrl'
+      controller: 'RouterCtrl',
+      restrict: 'E',
+      scope: {
+        routerData: '='
+      }
     }
   })
   .controller('RouterCtrl', function ($scope) {
     console.log('router controller', $scope.routerData);
+    $scope.$watch('routerData', function (a,b,c) {
+      console.log('$scope.routerData', $scope.routerData);
+      console.log('a',a);
+      console.log('b',b);
+      console.log('c',c);
+    }, true);
   })
 ;
