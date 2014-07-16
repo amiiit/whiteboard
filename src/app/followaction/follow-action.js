3515890779bc8e63fe51b1c814ca9a8a36e93009
Amit <jakubowicz.amit@gmail.com>
2014-07-16 16:54:26 +0200

    follow action works

--- src/app/followaction/follow-action.js
+++ src/app/followaction/follow-action.js
@@ -13,18 +13,34 @@ angular.module('nuBoard')
       controller: 'FollowActionCtrl'
     }
   })
-  .controller('FollowActionCtrl', function (RouterService) {
+  .controller('FollowActionCtrl', function (RouterService, $scope, UserService) {
 
     console.log('FollowActionCtrl');
 
-    var focusOnAction = function(action){
-      console.log('focus on action', action);
+    var ownUserId = UserService.id();
+
+    var focusOnAction = function (action) {
+      var points = action.points;
+      var lastPoint = [points[points.length - 2], points[points.length - 1]];
+
+      var focusPoint = {
+        x: lastPoint[0] / $scope.surfaceWidth(),
+        y: lastPoint[1] / $scope.surfaceHeight()
+      };
+
+      console.log('focus on', focusPoint);
+
+      $scope.relativeFocus = focusPoint;
+
+
     };
 
     RouterService.register({
       instanceId: 'follow-action',
       callback: function (action) {
-        focusOnAction(action);
+        if (action.sourceId != ownUserId) {
+          focusOnAction(action);
+        }
       }
     });
   })
